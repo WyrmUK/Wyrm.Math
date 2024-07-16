51cf76728039ee0d6cad5da7a0fe2be758871677
Laurence Mee <lmee@wyrm.org.uk>
2024-07-16 10:44:16 +0200

    Initial library with Matrix operations.

---
+++ src/Wyrm.Math/Matrix/GeneralMatrixDouble.cs
@@ -0,0 +1,188 @@
+﻿namespace Wyrm.Math.Matrix;
+
+/// <summary>
+/// A <see cref="GeneralMatrix{T}"/> for <see cref="double"/> values.
+/// </summary>
+public sealed class GeneralMatrixDouble : IGeneralMatrix<double>
+{
+    private readonly GeneralMatrix<double> _matrix;
+
+    /// <summary>
+    /// Creates a new empty <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="columns">The number of columns.</param>
+    /// <param name="rows">The number of rows.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the columns or rows are 0 or negative.</exception>
+    public GeneralMatrixDouble(int columns, int rows)
+    {
+        _matrix = new GeneralMatrix<double>(columns, rows);
+    }
+
+    /// <summary>
+    /// Creates a new populated <see cref="GeneralMatrixDouble"/>.
+    /// The rows and columns are set from the values.
+    /// Accepts column values of different lengths - the number of columns will be the largest length.
+    /// </summary>
+    /// <param name="values">The values to populate it with (an <see cref="IEnumerable{T}"/> of <see cref="IEnumerable{T}"/> column values).</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown if the columns or rows are 0.</exception>
+    public GeneralMatrixDouble(IEnumerable<IEnumerable<double>> values)
+    {
+        _matrix = new GeneralMatrix<double>(values);
+    }
+
+    /// <summary>
+    /// Creates a new populated <see cref="GeneralMatrixDouble"/>.
+    /// This will be a copy of the source matrix.
+    /// </summary>
+    /// <param name="matrix">The matrix to copy from.</param>
+    public GeneralMatrixDouble(IGeneralMatrix<double> matrix)
+    {
+        _matrix = new GeneralMatrix<double>(matrix.Values);
+    }
+
+    /// <inheritdoc/>
+    public int Rows => _matrix.Rows;
+
+    /// <inheritdoc/>
+    public int Columns => _matrix.Columns;
+
+    /// <inheritdoc/>
+    public IEnumerable<IEnumerable<double>> Values => _matrix.Values;
+
+    /// <inheritdoc/>
+    public double this[int column, int row]
+    {
+        get => _matrix[column, row];
+        set => _matrix[column, row] = value;
+    }
+
+    /// <summary>
+    /// Transposes the matrix.
+    /// </summary>
+    /// <returns>A new <see cref="GeneralMatrixDouble"/> with transposed values.</returns>
+    public GeneralMatrixDouble Transpose() => new(_matrix.Transpose());
+
+    /// <summary>
+    /// Returns the Trace of a square matrix (same number of columns as rows).
+    /// </summary>
+    /// <returns>The Trace.</returns>
+    /// <exception cref="ArgumentException">Throw if the matrix isn't square.</exception>
+    public double Trace() => _matrix.Trace((v1, v2) => v1 + v2);
+
+    /// <summary>
+    /// Adds a scalar value to each value of a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="scalar">Right hand <see cref="double"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the sum of the two operands.</returns>
+    public static GeneralMatrixDouble operator +(GeneralMatrixDouble m, double scalar) =>
+        new(m._matrix.PerformOperation(v => v + scalar));
+
+    /// <summary>
+    /// Adds a scalar value to each value of a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="scalar">Left hand <see cref="double"/>.</param>
+    /// <param name="m">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the sum of the two operands.</returns>
+    public static GeneralMatrixDouble operator +(double scalar, GeneralMatrixDouble m) =>
+        new(m._matrix.PerformOperation(v => v + scalar));
+
+    /// <summary>
+    /// Adds two <see cref="GeneralMatrixDouble"/>s together.
+    /// </summary>
+    /// <param name="m1">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="m2">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the sum of the two operands.</returns>
+    /// <exception cref="ArgumentException">Thrown when the shapes of the two matrices differ.</exception>
+    public static GeneralMatrixDouble operator +(GeneralMatrixDouble m1, GeneralMatrixDouble m2) =>
+        new(m1._matrix.PerformOperation(m2._matrix, (v1, v2) => v1 + v2));
+
+    /// <summary>
+    /// Generates a new <see cref="GeneralMatrixDouble"/> from an existing one.
+    /// </summary>
+    /// <param name="m">The <see cref="GeneralMatrixDouble"/> to copy from.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> with the values of the operand.</returns>
+    public static GeneralMatrixDouble operator +(GeneralMatrixDouble m) =>
+        new(m._matrix);
+
+    /// <summary>
+    /// Subtracts a single value from each value of a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="scalar">Right hand <see cref="double"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand minus the right hand value.</returns>
+    public static GeneralMatrixDouble operator -(GeneralMatrixDouble m, double scalar) =>
+        new(m._matrix.PerformOperation(v => v - scalar));
+
+    /// <summary>
+    /// Subtracts each value of a <see cref="GeneralMatrixDouble"/> from a scalar.
+    /// </summary>
+    /// <param name="scalar">Left hand <see cref="double"/>.</param>
+    /// <param name="m">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand minus the right hand value.</returns>
+    public static GeneralMatrixDouble operator -(double scalar, GeneralMatrixDouble m) =>
+        new(m._matrix.PerformOperation(v => scalar - v));
+
+    /// <summary>
+    /// Subtracts one <see cref="GeneralMatrixDouble"/> from another.
+    /// </summary>
+    /// <param name="m1">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="m2">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the left hand operand minus the right hand operand.</returns>
+    /// <exception cref="ArgumentException">Thrown when the shapes of the two matrices differ.</exception>
+    public static GeneralMatrixDouble operator -(GeneralMatrixDouble m1, GeneralMatrixDouble m2) =>
+        new(m1._matrix.PerformOperation(m2._matrix, (v1, v2) => v1 - v2));
+
+    /// <summary>
+    /// Negates all values in a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m">The <see cref="GeneralMatrixDouble"/> to negate.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> which is the negated operand.</returns>
+    public static GeneralMatrixDouble operator -(GeneralMatrixDouble m) =>
+        new(m._matrix.PerformOperation(v => -v));
+
+    /// <summary>
+    /// Multiplies each value of a <see cref="GeneralMatrixDouble"/> with a scalar.
+    /// </summary>
+    /// <param name="m">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="scalar">Right hand <see cref="double"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand multiplied by the right hand value.</returns>
+    public static GeneralMatrixDouble operator *(GeneralMatrixDouble m, double scalar) =>
+        new(m._matrix.PerformOperation(v => v * scalar));
+
+    /// <summary>
+    /// Multiplies a scalar with each value of a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m1">Left hand <see cref="double"/>.</param>
+    /// <param name="m2">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand multiplied by the right hand value.</returns>
+    public static GeneralMatrixDouble operator *(double m1, GeneralMatrixDouble m2) =>
+        new(m2._matrix.PerformOperation(v => m1 * v));
+
+    /// <summary>
+    /// Multiplies each value of a <see cref="GeneralMatrixDouble"/> with another <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m1">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="m2">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand multiplied by the right hand value.</returns>
+    public static GeneralMatrixDouble operator *(GeneralMatrixDouble m1, GeneralMatrixDouble m2) =>
+        new(m1._matrix.PerformMultiplyOperation(m2._matrix, (v1, v2) => v1 * v2, (v1, v2) => v1 + v2));
+
+    /// <summary>
+    /// Divides a each value of a <see cref="GeneralMatrixDouble"/> with a scalar.
+    /// </summary>
+    /// <param name="m">Left hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <param name="scalar">Right hand <see cref="double"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand divided by the right hand value.</returns>
+    public static GeneralMatrixDouble operator /(GeneralMatrixDouble m, double scalar) =>
+        new(m._matrix.PerformOperation(v => v / scalar));
+
+    /// <summary>
+    /// Divides a scalar with each value of a <see cref="GeneralMatrixDouble"/>.
+    /// </summary>
+    /// <param name="m1">Left hand <see cref="double"/>.</param>
+    /// <param name="m2">Right hand <see cref="GeneralMatrixDouble"/>.</param>
+    /// <returns>A <see cref="GeneralMatrixDouble"/> of the of the left hand operand divided by the right hand value.</returns>
+    public static GeneralMatrixDouble operator /(double m1, GeneralMatrixDouble m2) =>
+        new(m2._matrix.PerformOperation(v => m1 / v));
+}
---
+++ tests/Wyrm.Math.UnitTests/Matrix/GeneralMatrixDoubleTests.cs
@@ -0,0 +1,314 @@
+﻿using Shouldly;
+using Wyrm.Math.Matrix;
+
+namespace Wyrm.Math.UnitTests.Matrix;
+
+public class GeneralMatrixDoubleTests
+{
+    [Theory]
+    [InlineData(-1, 1)]
+    [InlineData(1, -1)]
+    public void Constructor_Should_Throw_ArgumentOutOfRangeException_If_Parameter_Negative(int columns, int rows)
+    {
+        Should.Throw<ArgumentOutOfRangeException>(() => new GeneralMatrixDouble(columns, rows));
+    }
+
+    [Fact]
+    public void Constructor_Should_Create_Empty_Matrix()
+    {
+        Should.NotThrow(() => new GeneralMatrixDouble(0, 0));
+    }
+
+    [Theory]
+    [InlineData(3, 2)]
+    public void Constructor_Should_Create_Initialised_Matrix(int columns, int rows)
+    {
+        new GeneralMatrixDouble(columns, rows).Values
+            .Sum(v => v.Where(d => d == 0.0).Count()).ShouldBe(columns * rows);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Constructor_Should_Create_Populated_Matrix(IEnumerable<IEnumerable<double>> values)
+    {
+        new GeneralMatrixDouble(values).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(values.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Constructor_Should_Create_Copy_Matrix(IEnumerable<IEnumerable<double>> values)
+    {
+        new GeneralMatrixDouble(new GeneralMatrixDouble(values)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(values.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Rows_Should_Get_Rows(IEnumerable<IEnumerable<double>> values)
+    {
+        new GeneralMatrixDouble(values).Rows
+            .ShouldBe(values.Count());
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Columns_Should_Get_Columns(IEnumerable<IEnumerable<double>> values)
+    {
+        new GeneralMatrixDouble(values).Columns
+            .ShouldBe(values.Max(v => v.Count()));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestGeneralMatrixTheoryData))]
+    public void ArraySetter_Should_Set_ColumnValue(GeneralMatrixDouble matrix)
+    {
+        matrix[2, 1] = TestValue;
+        matrix[2, 1].ShouldBe(TestValue);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestGeneralMatrixTransposeTheoryData))]
+    public void Transpose_Should_Transpose_Matrix(GeneralMatrixDouble matrix, GeneralMatrixDouble expected)
+    {
+        matrix.Transpose().Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.Values.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [InlineData(2, 3)]
+    public void Trace_Should_Throw_ArgumentException_If_Matrix_Not_Square(int columns, int rows)
+    {
+        Should.Throw<ArgumentException>(() => new GeneralMatrixDouble(columns, rows).Trace());
+    }
+
+    [Theory]
+    [MemberData(nameof(TestGeneralMatrixTraceTheoryData))]
+    public void Trace_Should_Return_Trace_Of_Matrix(GeneralMatrixDouble matrix, double expected)
+    {
+        matrix.Trace().ShouldBe(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleAdditionTheoryData))]
+    public void Operator_Add_Should_Add_Double_To_Matrix(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m) + value).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleAdditionTheoryData))]
+    public void Operator_Add_Should_Add_Matrix_To_Double(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (value + new GeneralMatrixDouble(m)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [InlineData(3, 2, 2, 2)]
+    [InlineData(3, 2, 3, 3)]
+    public void Operator_Add_Should_Throw_ArgumentException_If_Rows_Or_Columns_Mismatch(int columns1, int rows1, int columns2, int rows2)
+    {
+        Should.Throw<ArgumentException>(() => new GeneralMatrixDouble(columns1, rows1) + new GeneralMatrixDouble(columns2, rows2));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestAdditionTheoryData))]
+    public void Operator_Add_Should_Add_Matrices(IEnumerable<IEnumerable<double>> m1, IEnumerable<IEnumerable<double>> m2, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m1) + new GeneralMatrixDouble(m2)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Operator_Copy_Should_Copy_Matrix(IEnumerable<IEnumerable<double>> values)
+    {
+        (+new GeneralMatrixDouble(values)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(values.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleSubtractionTheoryData))]
+    public void Operator_Subtract_Should_Subtract_Double_From_Matrix(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m) - value).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestSubtractionDoubleTheoryData))]
+    public void Operator_Subtract_Should_Subtract_Matrix_From_Double(double value, IEnumerable<IEnumerable<double>> m, IEnumerable<IEnumerable<double>> expected)
+    {
+        (value - new GeneralMatrixDouble(m)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [InlineData(3, 2, 2, 2)]
+    [InlineData(3, 2, 3, 3)]
+    public void Operator_Subtract_Should_Throw_ArgumentException_If_Rows_Or_Columns_Mismatch(int columns1, int rows1, int columns2, int rows2)
+    {
+        Should.Throw<ArgumentException>(() => new GeneralMatrixDouble(columns1, rows1) - new GeneralMatrixDouble(columns2, rows2));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestSubtractionTheoryData))]
+    public void Operator_Subtract_Should_Add_Matrices(IEnumerable<IEnumerable<double>> m1, IEnumerable<IEnumerable<double>> m2, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m1) - new GeneralMatrixDouble(m2)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMatrixTheoryData))]
+    public void Operator_Negate_Should_Negate_Matrix(IEnumerable<IEnumerable<double>> values)
+    {
+        (-new GeneralMatrixDouble(values)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(values.SelectMany(v => v.Select(d => -d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleMultiplicationTheoryData))]
+    public void Operator_Multiply_Should_Multiply_Double_From_Matrix(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m) * value).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleMultiplicationTheoryData))]
+    public void Operator_Multiply_Should_Multiply_Matrix_From_Double(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (value * new GeneralMatrixDouble(m)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [InlineData(3, 2, 3, 2)]
+    [InlineData(3, 2, 3, 4)]
+    public void Operator_Multiply_Should_Throw_ArgumentException_If_Rows_And_Columns_Mismatch(int columns1, int rows1, int columns2, int rows2)
+    {
+        Should.Throw<ArgumentException>(() => new GeneralMatrixDouble(columns1, rows1) * new GeneralMatrixDouble(columns2, rows2));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestMultiplicationTheoryData))]
+    public void Operator_Multiple_Should_Multiple_Matrices(IEnumerable<IEnumerable<double>> m1, IEnumerable<IEnumerable<double>> m2, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m1) * new GeneralMatrixDouble(m2)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDoubleDivisionTheoryData))]
+    public void Operator_Divide_Should_Divide_Matrix_By_Double(IEnumerable<IEnumerable<double>> m, double value, IEnumerable<IEnumerable<double>> expected)
+    {
+        (new GeneralMatrixDouble(m) / value).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    [Theory]
+    [MemberData(nameof(TestDivisionDoubleTheoryData))]
+    public void Operator_Divide_Should_Divide_Double_By_Matrix(double value, IEnumerable<IEnumerable<double>> m, IEnumerable<IEnumerable<double>> expected)
+    {
+        (value / new GeneralMatrixDouble(m)).Values
+            .SelectMany(v => v.Select(d => d))
+            .ShouldBeEquivalentTo(expected.SelectMany(v => v.Select(d => d)));
+    }
+
+    #region Test Data
+
+    public const double TestValue = 10.1;
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>> TestMatrixTheoryData =
+        new([[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]]);
+
+    public static readonly TheoryData<GeneralMatrixDouble> TestGeneralMatrixTheoryData =
+        new(new GeneralMatrixDouble([[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]]));
+
+    public static readonly TheoryData<GeneralMatrixDouble, GeneralMatrixDouble> TestGeneralMatrixTransposeTheoryData =
+        new()
+        {
+            { new GeneralMatrixDouble([[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]]), new GeneralMatrixDouble([[1.1, 4.4], [2.2, 5.5], [3.3, 6.6]]) }
+        };
+
+    public static readonly TheoryData<GeneralMatrixDouble, double> TestGeneralMatrixTraceTheoryData =
+        new()
+        {
+            { new GeneralMatrixDouble([[1.1, 2.2], [3.3, 4.4]]), 1.1 + 4.4 }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, double, IEnumerable<IEnumerable<double>>> TestDoubleAdditionTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], 20.2, [[1.1 + 20.2, 2.2 + 20.2, 3.3 + 20.2], [4.4 + 20.2, 5.5 + 20.2, 6.6 + 20.2]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>> TestAdditionTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], [[2.2, 3.3, 4.4], [5.5, 6.6, 7.7]], [[1.1 + 2.2, 2.2 + 3.3, 3.3 + 4.4], [4.4 + 5.5, 5.5 + 6.6, 6.6 + 7.7]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, double, IEnumerable<IEnumerable<double>>> TestDoubleSubtractionTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], 20.2, [[1.1 - 20.2, 2.2 - 20.2, 3.3 - 20.2], [4.4 - 20.2, 5.5 - 20.2, 6.6 - 20.2]] }
+        };
+
+    public static readonly TheoryData<double, IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>> TestSubtractionDoubleTheoryData =
+        new()
+        {
+            { 20.2, [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], [[20.2 - 1.1, 20.2 - 2.2, 20.2 - 3.3], [20.2 - 4.4, 20.2 - 5.5, 20.2 - 6.6]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>> TestSubtractionTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], [[2.2, 3.3, 4.4], [5.5, 6.6, 7.7]], [[1.1 - 2.2, 2.2 - 3.3, 3.3 - 4.4], [4.4 - 5.5, 5.5 - 6.6, 6.6 - 7.7]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, double, IEnumerable<IEnumerable<double>>> TestDoubleMultiplicationTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], 20.2, [[1.1 * 20.2, 2.2 * 20.2, 3.3 * 20.2], [4.4 * 20.2, 5.5 * 20.2, 6.6 * 20.2]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>> TestMultiplicationTheoryData =
+        new()
+        {
+            { [[1.1, 2.2], [3.3, 4.4]], [[5.5, 6.6], [7.7, 8.8]], [[1.1 * 5.5 + 2.2 * 7.7, 1.1 * 6.6 + 2.2 * 8.8], [3.3 * 5.5 + 4.4 * 7.7, 3.3 * 6.6 + 4.4 * 8.8]] },
+            { [[-1.0, 2.0, 3.0], [4.0, 0.0, 5.0]], [[5.0, -1.0], [-4.0, 0.0], [2.0, 3.0]], [[-1.0 * 5.0 + 2.0 * -4.0 + 3.0 * 2.0, -1.0 * -1.0 + 2.0 * 0.0 + 3.0 * 3.0], [4.0 * 5.0 + 0.0 * -4.0 + 5.0 * 2.0, 4.0 * -1.0 + 0.0 * 0.0 + 5.0 * 3.0]] }
+        };
+
+    public static readonly TheoryData<IEnumerable<IEnumerable<double>>, double, IEnumerable<IEnumerable<double>>> TestDoubleDivisionTheoryData =
+        new()
+        {
+            { [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], 20.2, [[1.1 / 20.2, 2.2 / 20.2, 3.3 / 20.2], [4.4 / 20.2, 5.5 / 20.2, 6.6 / 20.2]] }
+        };
+
+    public static readonly TheoryData<double, IEnumerable<IEnumerable<double>>, IEnumerable<IEnumerable<double>>> TestDivisionDoubleTheoryData =
+        new()
+        {
+            { 20.2, [[1.1, 2.2, 3.3], [4.4, 5.5, 6.6]], [[20.2 / 1.1, 20.2 / 2.2, 20.2 / 3.3], [20.2 / 4.4, 20.2 / 5.5, 20.2 / 6.6]] }
+        };
+
+    #endregion
+}
