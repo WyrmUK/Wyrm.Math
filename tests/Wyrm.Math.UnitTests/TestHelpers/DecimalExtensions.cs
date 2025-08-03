46a32dd4c1c6df5ea0345443ec2ad413a141bd37
Laurence Mee <lmee@wyrm.org.uk>
2025-08-04 00:17:41 +0100

    Using defined tolerances.

--- tests/Wyrm.Math.UnitTests/TestHelpers/DecimalExtensions.cs
+++ tests/Wyrm.Math.UnitTests/TestHelpers/DecimalExtensions.cs
@@ -4,19 +4,14 @@ namespace Wyrm.Math.UnitTests.TestHelpers;
 
 public static class DecimalExtensions
 {
-    public static void ShouldBeWithinTolerance(this decimal value, decimal expected, int lsds = 1)
+    public static void ShouldBeWithinTolerance(this decimal value, decimal expected, decimal tolerance)
     {
-        var tolerance = lsds * 0.0000000000000000000000000001M;
         if (tolerance == 0M)
         {
             value.ShouldBe(expected);
         }
         else
         {
-            if (expected.Abs() >= 10.0M)
-            {
-                tolerance *= (decimal)10.0.Pow(((double)expected.Abs()).Log10().Round(MidpointRounding.ToZero));
-            }
             value.ShouldBeGreaterThanOrEqualTo(expected - tolerance);
             value.ShouldBeLessThanOrEqualTo(expected + tolerance);
         }
