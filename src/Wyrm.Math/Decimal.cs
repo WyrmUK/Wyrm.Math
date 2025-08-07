c44792af78b47972e299bc2bc332515c9df05649
Laurence Mee <lmee@wyrm.org.uk>
2025-08-08 01:16:52 +0200

    Added tolerance to Exp unit tests.

--- src/Wyrm.Math/Decimal.cs
+++ src/Wyrm.Math/Decimal.cs
@@ -19,7 +19,7 @@ public static class Decimal
     internal const decimal Exp2 = 7.389056098930650227230427460575007813180315570551847324087M;
     internal const decimal MaxExpPow = 66.542129333754749704054283659M;
     internal const decimal ExpNeg2 = 0.1353352832366126918939994949724844034076315459095759M;
-    internal const decimal MinExpPow = -65.386294361119890618834464243M;
+    internal const decimal MinExpPow = -64.472382603833279152503760731M;
     internal const decimal ExpNeg1 = 0.3678794411714423215955237701614608674458111310317678M;
     internal const decimal MaxSqrVal = 281474976710655M;
     internal const decimal Log2 = 0.6931471805599453094172321218M;
